5c93cf5901eab72b1691f1521b0312d66e98ef20
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-12-13 19:24:37 +0000

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,58 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const TodoList = () => {
+    const [tasks, setTasks] = useState([]);
+    const [taskInput, setTaskInput] = useState('');
+  
+    const handleInputChange = (e) => {
+      setTaskInput(e.target.value);
+    };
+  
+    const addTask = () => {
+      if (taskInput.trim() !== '') {
+        setTasks([...tasks, taskInput]);
+        setTaskInput('');
+      } else {
+        alert('Please enter a task!');
+      }
+    };
+  
+    const deleteTask = (index) => {
+      const updatedTasks = tasks.filter((_, i) => i !== index);
+      setTasks(updatedTasks);
+    };
+  
+    return (
+      <div className="todo-container">
+        <h1>To-Do List</h1>
+        <input
+          type="text"
+          value={taskInput}
+          onChange={handleInputChange}
+          placeholder="Enter a new task"
+        />
+        <button onClick={addTask}>Add Todo</button>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}
+              <button onClick={() => deleteTask(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+    
+    
   return (
     <div>
         {/* Do not remove the main div */}
+        <TodoList/>
     </div>
   )
 }
